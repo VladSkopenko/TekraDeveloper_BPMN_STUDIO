37d25bcbf42b5bc93a0c0ce5b8dc22797290b76e
VladSkop <skopirka2k17@gmail.com>
2026-01-18 19:41:34 +0200

    clean code

--- StudioTekra/e2e/app.spec.ts
+++ StudioTekra/e2e/app.spec.ts
@@ -1,16 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('workspace-project App', () => {
-
   test('should display diagram', async ({ page }) => {
-
-    // when
     await page.goto('/');
-
-    // then
     const element = await page.$('app-root .diagram-container');
-
     expect(element).not.toBeNull();
   })
-
 });
